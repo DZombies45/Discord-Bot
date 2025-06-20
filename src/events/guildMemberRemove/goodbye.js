af245f8e332a3e5ad51eb1a95fadbfccc971cc52
Dzombies45 <dewan.dinda@gmail.com>
2025-06-20 19:18:49 +0700

    change welcome goodbye ing

--- src/events/guildMemberRemove/goodbye.js
+++ src/events/guildMemberRemove/goodbye.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder, AuditLogEvent } = require("discord.js");
 const { welcome } = require("../../config.json");
-const { welcomeMessage } = require("../../messageConfig.json");
+const { goodbyeMessage } = require("../../messageConfig.json");
 const WelcomeLeave = require("../../utils/welcomeBye.js");
 
 module.exports = async (client, member) => {
@@ -8,8 +8,8 @@ module.exports = async (client, member) => {
     const channel = await member.guild.channels.cache.get(welcome.ch);
     if (!channel) return;
 
-    const msg = welcomeMessage[
-      Math.floor(Math.random() * welcomeMessage.length)
+    const msg = goodbyeMessage[
+      Math.floor(Math.random() * goodbyeMessage.length)
     ].replace(/<user>/g, member.user.username);
 
     const img = await new WelcomeLeave()
@@ -18,16 +18,16 @@ module.exports = async (client, member) => {
       )
       .setBackground(
         "image",
-        "https://cdn.discordapp.com/attachments/1046721681445638224/1241817497238241473/hyperion_bridge.png?ex=68564eeb&is=6854fd6b&hm=d9420c671bb265175ee1b0a07dc7514f8611d284f972e19cdf6414e570a2881d&",
+        "https://cdn.discordapp.com/attachments/1046721681445638224/1241817497754271785/Hyperion_mount.png?ex=68564eeb&is=6854fd6b&hm=e326318e7f152d18fa0b4ec373df29cf78762504c3237c14c6ea7bb2770c1f57&",
       )
       .setTitle(member.displayName)
-      .setDescription(msg, "#37A433")
-      .setBorder("#37A433")
+      .setDescription(msg, "#C21534")
+      .setBorder("#C21534")
       .setAvatarBorder("#703B98")
       .setOverlayOpacity(0.5)
       .build();
 
-    await channel.send({ files: [{ attachment: img, name: "on_bridge.png" }] });
+    await channel.send({ files: [{ attachment: img, name: "on_field.png" }] });
   } catch (error) {
     console.error(error);
   }

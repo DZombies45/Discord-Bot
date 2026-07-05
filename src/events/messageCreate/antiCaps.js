65ee3488c041efedae90d69f0289ad3025f7de14
jombie <dewan.dinda@gmail.com>
2026-07-05 13:38:00 +0700

    add some error

--- src/events/messageCreate/antiCaps.js
+++ src/events/messageCreate/antiCaps.js
@@ -1,21 +1,29 @@
+import { Logger } from "../../util.js";
+
 /**
  * @param {import("discord.js").Client} client
  * @param {import("discord.js").Message} message
  */
 export default async (client, message) => {
-  if (!message.guild || message.author.bot) return;
-  let msg = message.content;
-  if (msg?.length <= 5) return;
-  if (
-    msg.trim().match(/[A-Z]/g).length / msg.trim().replaceAll(" ", "").length <
-    0.8
-  )
-    return;
-  //do something
-  const a = await message
-    .reply("❗ **message contains too many uppercase letters** ❗")
-    .catch(() => {});
-  await message.delete().catch(() => {});
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  await a.delete().catch(() => {});
+  try {
+    // code
+    if (!message.guild || message.author.bot) return;
+    let msg = message.content;
+    if (msg?.length <= 5) return;
+    if (
+      msg.trim().match(/[A-Z]/g).length /
+        msg.trim().replaceAll(" ", "").length <
+      0.8
+    )
+      return;
+    //do something
+    const a = await message
+      .reply("❗ **message contains too many uppercase letters** ❗")
+      .catch(() => {});
+    await message.delete().catch(() => {});
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await a.delete().catch(() => {});
+  } catch (err) {
+    Logger.error(err);
+  }
 };
